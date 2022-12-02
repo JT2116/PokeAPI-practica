4859c51c00d38fc3fc160912397352e171a06c1f
Juan Torres <jtorres@wind.com.do>
2022-12-02 15:54:10 -0400

    primer commit

---
+++ app/helpers/poke_api.js
@@ -0,0 +1,11 @@
+const NAME = "pokemon",
+DOMAIN = `https://pokeapi.co`,
+API_POKE = `${DOMAIN}/api/v2/`,
+POKEMON = `${API_POKE}/pokemon`;
+
+export default {
+    NAME,
+    DOMAIN,
+    API_POKE,
+    POKEMON
+}
---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { App } from "./App.js";
+import api from "./helpers/poke_api.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded",App);
+window.addEventListener("hashchange",() => {
+    App();
+});
